591d073cd64614c6d5d2d743c69f7c7f4429b132
jephersonnn <ericjeffrey.sch@gmail.com>
2022-09-09 10:53:11 +0800

    Add Insert algorithms with MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //jshint esversion:6
+//Todo List with MongoDB Exercise
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose"); //mongoose node module import
 const date = require(__dirname + "/date.js"); //importing local node module
 
 const app = express();
@@ -16,13 +18,49 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 app.set('set engine', '.ejs'); //this tells Express to use EJS as a new view engine
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemSchema = { //Schema
+  name: {
+    type: String,
+    required: true
+  }
+};
+
+const Item = mongoose.model("Item", itemSchema); //Making a model out of the schema
+
+const itemA = new Item({
+  name: "This is your todo list!"
+})
+
+const itemB = new Item({
+  name: "Click + to add an item"
+})
+
+const itemC = new Item({
+  name: "<-- Cross out items by using the checkbox"
+})
+
 app.get("/", function(req, res) {
 
   let day = date.getDate();
 
-  res.render("list.ejs", {
-    listTitle: day,
-    newTodoItems: mainItems
+  Item.find({}, function(err, foundItems) {
+
+    if (foundItems.length === 0) {
+      Item.insertMany([itemA, itemB, itemC], function(err) {
+        if (err)
+          console.log("Oops something went wrong!");
+        else
+          console.log("Default items added successfully!");
+      })
+      res.redirect("/");
+    } else {
+      res.render("list.ejs", {
+        listTitle: day,
+        newTodoItems: foundItems
+      })
+    }
   })
 })
 
@@ -30,19 +68,18 @@ app.listen(port, function() {
   console.log("Server started on port " + port);
 })
 
-app.get("/about", function (req,res){
+app.get("/about", function(req, res) {
   res.render("about.ejs"); //render about.ejs
 })
 //any element href with the same .get parameter can trigger this. shall that element be clicked, .get block will be triggered, this rendering the about.ejs file
 
 app.post("/", function(req, res) {
-  if (req.body.list === "Work") { //on the <input> element found on list.ejs, there's an attribute that actually holds the data of listTitle. it uses that field to control where the new item should be pushed
-    workItems.push(req.body.itemTextField);
-    res.redirect("/work"); //redirect to the work page
-  } else {
-    mainItems.push(req.body.itemTextField);
-    res.redirect("/") //redirect to main page
-  }
+  const newItem = new Item({
+    name: req.body.itemTextField
+  })
+
+  newItem.save();
+  res.redirect("/") //redirect to main page
 
 })
 
